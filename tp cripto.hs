737eb2bdc436ab9432193e29547ac1218e7e2e45
Nicolas Manuel Brana <branani@ar.ibm.com>
2017-10-18 17:45:26 -0300

    Encript and Decript working. Performance issues for 22 rounds.

--- tp cripto.hs
+++ tp cripto.hs
@@ -1,6 +1,7 @@
 
 import Data.Bits
 import Numeric
+import Data.Word
 import Data.Maybe (listToMaybe,fromJust)
 import Data.Char
 import Data.Text.Encoding
@@ -14,56 +15,67 @@ intToBit aInt = showIntAtBase 2 intToDigit aInt ""
 stringToArrayOfInt aStringBit = map digitToInt aStringBit
 
 bitToInt :: Integral a => String ->  a
-bitToInt = fromJust.fmap fst . listToMaybe . readInt 2 (`elem` "01") digitToInt
+bitToInt = fromJust.fmap fst.listToMaybe.readInt 2 (`elem` "01") digitToInt
 
 completeBits cant stringBits 
-	|length stringBits<cant= (head (reverse (take (cant+1-(length stringBits)) (iterate (++"0") "")))) ++ stringBits
-	|otherwise=stringBits
+	|length stringBits < cant = (head (reverse (take (cant+1-(length stringBits)) (iterate (++"0") "")))) ++ stringBits
+	|otherwise = stringBits
 
-completeByte stringAscii= completeBits  8 stringAscii
+completeByte stringAscii = completeBits  8 stringAscii
 
-stringToAsciiChain aString=foldl1 (++) (map (completeByte.intToBit.ord) aString)
-stringToNumber:: (Integral a)=> String->a
-stringToNumber aString= bitToInt (stringToAsciiChain aString)
+stringToAsciiChain aString = foldl1 (++) (map (completeByte.intToBit.ord) aString)
+stringToNumber :: (Integral a) => String -> a
+stringToNumber aString = bitToInt (stringToAsciiChain aString)
 
 arrayRotate :: Int -> [a] -> [a]
 arrayRotate _ [] = []
 arrayRotate n xs 
-			|n>0 = ( take (length xs). (reverse (take n (reverse xs)) ++)) xs
-			|n<0 = ( take (length xs). ((drop (n * (-1)) xs) ++))  xs
+	|n > 0 = ( take (length xs). (reverse (take n (reverse xs)) ++)) xs
+	|n < 0 = ( take (length xs). ((drop (n * (-1)) xs) ++))  xs
 
-bitRotate rotation number size=  bitToInt (arrayRotate rotation (completeBits  size (intToBit number)))
+stringHexToBit string = foldl (++) "" (map (completeBits 4.intToBit) (map digitToInt string))
 
-
-
-additionModule value1 value2 moduleValue= mod (value1 + value2) moduleValue
+stringHexToNumeric string = bitToInt (stringHexToBit string)
 
 
 ----------------------------------------------------------------
+additionSpec :: Word16 -> Word16 -> Word16
+additionSpec aWord anotherWord = aWord + anotherWord
+substraccionSpeck :: Word16 -> Word16 -> Word16
+substraccionSpeck aWord anotherWord = aWord - anotherWord
 
-additionSpec value1 value2 = additionModule value1  value2  (2^16)
+rotate16 :: Int -> Word16 -> Word16
+rotate16 anAmount aWord = (rotate aWord.negate) anAmount
 
-stringHexToBit string=foldl (++) "" (map (completeBits 4.intToBit) (map digitToInt string))
 
-stringHexToNumeric string= bitToInt (stringHexToBit string)
 
-s_a value=bitRotate (8) value 16
-s_b value=bitRotate (-3) value 16
+rotate8ToRight :: Word16 -> Word16
+rotate8ToRight = rotate16 8
 
-l:: (Num a, Show a,Integral a, Bits a)=> [a]->a->a
+rotate3ToLeft :: Word16 -> Word16
+rotate3ToLeft = rotate16 (-3)
+
+l :: [Word16] -> Word16 -> Word16
 l keys t
-	|t>(m-2) = xor (additionSpec (k keys i) (s_a (l keys i))) ( i)
-	|t<=(m-2) = keys!!(fromIntegral t)
-	where i = t-m+1
-	      m = 4
+	|t > (m - 2) = xor (additionSpec (k keys i) (rotate16 8 (l keys i))) ( i)
+	|t <= (m - 2) = keys !! ((fromIntegral.toInteger) t)
+	where i = t - m + 1
+	      m = amountOfWordsForKey
 
-k:: (Num a, Show a,Integral a, Bits a)=> [a]->a->a
+k :: [Word16] -> Word16 -> Word16
 k keys t
-	|t==0 = keys!!(fromIntegral (m-1))
-	|otherwise = xor (s_b (k keys i)) (l keys (i+m-1))
-	where i = t-1
-	      m = 4
+	|t == 0 = keys !! ((fromIntegral.toInteger) (m - 1))
+	|otherwise = xor (rotate16 (-3) (k keys i)) (l keys (i + m - 1))
+	where i = t - 1
+	      m = amountOfWordsForKey
 
-speckEncriptingRound (lBlock,rBlock) key =((xor (additionSpec  (s_a lBlock) rBlock) key) ,xor (xor (additionSpec  (s_a lBlock) rBlock) key)  (s_b rBlock ))
+amountOfWordsForKey :: Word16
+amountOfWordsForKey = 4
 
-speckEncript (lBlock,rBlock) keys rounds=foldl (\(lBlock,rBlock) round->speckEncriptingRound (lBlock,rBlock) (k keys round)) (lBlock,rBlock)  [0..rounds] 
+speckEncriptingRound (lBlock,rBlock) key = ((xor (additionSpec  (rotate16 8 lBlock) rBlock) key) ,xor (xor (additionSpec  (rotate16 8 lBlock) rBlock) key)  (rotate16 (-3) rBlock ))
+
+speckEncript (lBlock,rBlock) keys rounds = foldl (\(lBlock,rBlock) round -> speckEncriptingRound (lBlock,rBlock) (k keys round)) (lBlock,rBlock)  [0..rounds] 
+
+speckDecriptRound (lBlock, rBlock) key = (rotate16 (-8) (substraccionSpeck (xor lBlock key) (rotate16 3 (xor lBlock rBlock))),rotate16 3 (xor lBlock rBlock))
+
+speckDecript (lBlock,rBlock) keys rounds = foldl (\(lBlock,rBlock) round -> speckDecriptRound (lBlock,rBlock) (k keys round)) (lBlock,rBlock)  (reverse [0..rounds])
