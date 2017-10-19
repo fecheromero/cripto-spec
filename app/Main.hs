585df0b64ec9fc6dc2ad6f6c0e29ff365a9bce25
Lucas David Traverso <lucas6246@gmail.com>
2017-10-19 02:23:28 -0300

    Ahora las cosas van mas rapido

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,13 @@
 module Main where
 
-import Crypto1
+-- import Crypto1
+-- import Crypto2
+import Crypto3
+import qualified Data.Vector as V
 
 main :: IO ()
 main = do
-    print $ (k [stringHexToNumeric "1918", stringHexToNumeric "1110", stringHexToNumeric "0908", stringHexToNumeric "0100"] 16 :: Int)
+    print (k (V.fromList [ stringHexToNumeric "1918"
+             , stringHexToNumeric "1110"
+             , stringHexToNumeric "0908"
+             , stringHexToNumeric "0100"]) 22)
